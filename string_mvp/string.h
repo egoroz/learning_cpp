392d07620c805069bb280d0d01b093d28b7d1c1a
egororo <egorsemirenko04@gmail.com>
2025-08-26 12:37:46 +0300

    v1

---
+++ string_mvp/string.h
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <cstring>
+//#include <algorithm> // for std::fill and std::copy
+
+class String{
+private:
+	char* str = nullptr;
+	size_t sz = 0;
+	size_t cap = 0;
+	static constexpr size_t DEFAULT_CAPACITY = 8;
+	static constexpr size_t CAPACITY_EXPANSION = 2;
+
+public:
+	explicit String(const char* other) {
+		if(!other){str = nullptr; sz = 0; cap = 0; return;}
+		sz = strlen(other);
+		cap = sz + 1;
+		str = new char[cap];
+		memcpy(str, other, sz + 1);
+	}
+
+	String() = default;
+
+	String(size_t n, char c): str(new char[n + 1]), sz(n), cap(n + 1){
+		memset(str, c, n);
+		str[n] = '\0';
+		if(c == '\0'){sz = 0;}
+	}
+
+	String(const String& other): str(new char[other.cap]), sz(other.sz), cap(other.cap){
+		memcpy(str, other.str, sz + 1);
+	} 
+
+	~String(){
+		delete[] str;
+	}
+
+	void swap(String& other){
+		std::swap(str, other.str);
+		std::swap(sz, other.sz);
+		std::swap(cap, other.cap);
+	}
+
+	// Copy and swap idiom
+	String& operator=(String other) & { // only lvalue
+		swap(other);
+		return *this;
+	}
+
+	// operators without implicit conversion
+	bool operator<(const String& other) const{
+		return strcmp(str, other.str) < 0;
+	}
+
+	bool operator>(const String& other) const{
+		return other < *this;
+	}
+
+	bool operator<=(const String& other) const{
+		return !(other < *this);
+	}
+
+	bool operator>=(const String& other) const{
+		return !(*this < other);
+	}
+
+	bool operator==(const String& other) const{
+		return strcmp(str, other.str) == 0;
+	}
+
+	bool operator!=(const String& other) const{
+		return strcmp(str, other.str) != 0;
+	}
+	
+
+	char& operator[](size_t n) {
+		return str[n];
+	}
+
+	const char& operator[](size_t n) const{
+		return str[n];
+	}
+	
+	size_t length() const{
+		return sz;
+	}
+
+	size_t size() const{
+		return sz;
+	}
+
+	size_t capacity() const{
+		return cap;
+	}
+
+	void push_back(char c){
+		if (cap == 0){
+			cap = DEFAULT_CAPACITY;
+			str = new char[cap];
+		}
+		if(sz + 1 == cap){
+			char* new_str = new char[cap *= CAPACITY_EXPANSION];
+			memcpy(new_str, str, sz + 1);
+			delete[] str;
+			str = new_str;
+		}
+		str[sz++] = c;
+		str[sz] = '\0';
+	}
+
+	void pop_back(){
+		str[--sz] = '\0';
+	}
+
+	char& front() {
+		return str[0];
+	}
+
+	const char& front() const {
+		return str[0];
+	}
+
+	char& back() {
+		return str[sz-1];
+	}
+
+	const char& back() const{
+		return str[sz-1];
+	}
+
+	String& operator+=(char c){
+		this->push_back(c);
+		return *this;
+	}
+
+	String& operator+=(const String& other){
+		if (cap == 0){
+			cap = (DEFAULT_CAPACITY < other.sz + 1) ? other.sz + 1 : DEFAULT_CAPACITY;
+			str = new char[cap];
+		}
+		if(sz + other.sz + 1 > cap){
+			cap = (cap * CAPACITY_EXPANSION < sz + other.sz + 1) ? sz + other.sz + 1 : cap * CAPACITY_EXPANSION;
+			char* new_str = new char[cap];
+			memcpy(new_str, str, sz);
+			delete[] str;
+			str = new_str;
+		}
+		memcpy(str + sz, other.str, other.sz + 1);	
+		return *this;
+	}
+
+	explicit String(char c): str(new char[2]), sz(1), cap(2){
+		str[1] = '\0';
+		if(c == '\0'){sz = 0;}
+	}
+
+	explicit String(size_t n): str(new char[n + 1]), sz(0), cap(n+1){
+		str[0] = '\0';
+	}
+
+	explicit operator const char*() const{
+		return str;
+	}
+	
+	size_t find(const String& substring) const{
+		const char* pos;
+		return (pos = strstr(str, static_cast<const char*>(substring))) ? static_cast<size_t>(pos - str) : sz;
+	}
+
+	size_t rfind(const String& substring) const{
+		const char* main_string = str + sz - substring.size();
+		while(strstr(main_string, static_cast<const char*>(substring)) == nullptr){
+			if(str == main_string--){return sz;}
+		}
+		return static_cast<size_t>(main_string - str);
+	}
+
+	String substr(size_t start, size_t count) const{
+		String substring(count);
+		memcpy(substring.str, str + start, count);
+		substring.sz = count;
+		substring[count] = '\0';
+		return substring;
+	}
+
+	bool empty() const{
+		return sz == 0;
+	}
+
+	void clear(){
+		if(!str){return;}
+		str[0] = '\0';
+		sz = 0;
+	}
+
+	void shrink_to_fit(){
+		if(sz + 1 == cap){return;}
+		cap = sz + 1;
+		char* new_str = new char[sz + 1];
+		memcpy(new_str, str, sz + 1);
+		delete[] str;
+		str = new_str;
+	}
+
+	char* data(){
+		return str;
+	}
+
+	const char* data() const{
+		return str;
+	}
+};
+
+
+String operator""_str(const char* str, size_t){
+	return String(str);
+}
+
+// useful compiler optimization
+String operator+(const String& a, const String& b){
+	String copy = a;
+	copy += b;
+	return copy;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& string){
+	if(!string.data()){return os;}
+	return os << string.data();
+}
+
+std::istream& operator>>(std::istream& is, String& string){
+	string.clear();
+	is >> std::ws;
+	while(is.good()){
+		int next_char = is.peek();
+		if(next_char == EOF || std::isspace(static_cast<unsigned char>(next_char))){
+			break;
+		}
+		char c;
+		is.get(c);
+		string.push_back(c);
+	}
+	return is;
+}
---
+++ string_mvp/string_tests.cpp
@@ -0,0 +1,229 @@
+#include "string.h"
+#include <gtest/gtest.h>
+#include <sstream>
+#include <limits>
+
+// ---------- Конструкторы ----------
+TEST(ConstructorTest, FromCString) {
+    String s("hello");
+    EXPECT_EQ(s.size(), 5);
+    EXPECT_STREQ(s.data(), "hello");
+}
+
+TEST(ConstructorTest, EmptyCString) {
+    String s("");
+    EXPECT_EQ(s.size(), 0);
+    EXPECT_STREQ(s.data(), "");
+}
+
+TEST(ConstructorTest, FillConstructorChar) {
+    String s(5, 'a');
+    EXPECT_EQ(s.size(), 5);
+    EXPECT_STREQ(s.data(), "aaaaa");
+}
+
+TEST(ConstructorTest, FillConstructorNullChar) {
+    String s(5, '\0');
+    EXPECT_EQ(s.size(), 0);  // должно обнулять
+    EXPECT_STREQ(s.data(), "");
+}
+
+TEST(ConstructorTest, CopyConstructor) {
+    String s1("copy");
+    String s2(s1);
+    EXPECT_EQ(s1, s2);
+    EXPECT_NE(s1.data(), s2.data()); // разные буферы
+}
+
+TEST(ConstructorTest, AssignmentOperator) {
+    String a("foo");
+    String b("bar");
+    b = a;
+    EXPECT_EQ(a, b);
+}
+
+TEST(ConstructorTest, DefaultConstructor) {
+    String s;
+    EXPECT_EQ(s.size(), 0);
+    EXPECT_TRUE(s.empty());
+}
+
+// ---------- Операторы сравнения ----------
+TEST(OperatorTest, ComparisonEqual) {
+    String a("abc"), b("abc");
+    EXPECT_TRUE(a == b);
+    EXPECT_FALSE(a != b);
+}
+
+TEST(OperatorTest, ComparisonDifferent) {
+    String a("abc"), b("abd");
+    EXPECT_TRUE(a != b);
+    EXPECT_TRUE(a < b);
+    EXPECT_TRUE(b > a);
+    EXPECT_TRUE(a <= b);
+    EXPECT_TRUE(b >= a);
+}
+
+// ---------- Доступ к символам ----------
+TEST(OperatorTest, Indexing) {
+    String s("hello");
+    EXPECT_EQ(s[0], 'h');
+    s[0] = 'H';
+    EXPECT_EQ(s[0], 'H');
+}
+
+TEST(MethodTest, FrontBack) {
+    String s("world");
+    EXPECT_EQ(s.front(), 'w');
+    EXPECT_EQ(s.back(), 'd');
+    s.back() = '!';
+    EXPECT_STREQ(s.data(), "worl!");
+}
+
+// ---------- Размер и вместимость ----------
+TEST(MethodTest, SizeCapacityLength) {
+    String s("test");
+    EXPECT_EQ(s.size(), 4);
+    EXPECT_EQ(s.length(), 4);
+    EXPECT_GE(s.capacity(), 5); // минимум size+1
+}
+
+// ---------- push_back / pop_back ----------
+TEST(MethodTest, PushBack) {
+    String s;
+    for (int i = 0; i < 20; i++) s.push_back('x');
+    EXPECT_EQ(s.size(), 20);
+    EXPECT_TRUE(s.capacity() >= 21);
+}
+
+TEST(MethodTest, PopBack) {
+    String s("abc");
+    s.pop_back();
+    EXPECT_EQ(s.size(), 2);
+    EXPECT_STREQ(s.data(), "ab");
+    s.pop_back();
+    s.pop_back();
+    EXPECT_EQ(s.size(), 0);
+    EXPECT_STREQ(s.data(), "");
+}
+
+// ---------- Операторы += и + ----------
+TEST(OperatorTest, PlusEqualChar) {
+    String s("a");
+    s += 'b';
+    EXPECT_STREQ(s.data(), "ab");
+}
+
+TEST(OperatorTest, PlusEqualString) {
+    String s1("abc"), s2("def");
+    s1 += s2;
+    EXPECT_STREQ(s1.data(), "abcdef");
+}
+
+TEST(OperatorTest, PlusOperator) {
+    String a("foo"), b("bar");
+    String c = a + b;
+    EXPECT_STREQ(c.data(), "foobar");
+}
+
+// ---------- find / rfind ----------
+TEST(MethodTest, FindExisting) {
+    String s("abracadabra");
+    EXPECT_EQ(s.find("bra"_str), 1);
+    EXPECT_EQ(s.find("abra"_str), 0);
+    EXPECT_EQ(s.rfind("bra"_str), 8);
+}
+
+TEST(MethodTest, FindNotFound) {
+    String s("hello");
+    EXPECT_EQ(s.find("xyz"_str), s.size());
+    EXPECT_EQ(s.rfind("123"_str), s.size());
+}
+
+// ---------- substr ----------
+TEST(MethodTest, SubstrMiddle) {
+    String s("abcdef");
+    String sub = s.substr(2, 3);
+    EXPECT_STREQ(sub.data(), "cde");
+}
+
+TEST(MethodTest, SubstrWhole) {
+    String s("abcdef");
+    String sub = s.substr(0, s.size());
+    EXPECT_STREQ(sub.data(), "abcdef");
+}
+
+TEST(MethodTest, SubstrZero) {
+    String s("abcdef");
+    String sub = s.substr(3, 0);
+    EXPECT_EQ(sub.size(), 0);
+}
+
+// ---------- empty / clear ----------
+TEST(MethodTest, EmptyClear) {
+    String s("abc");
+    EXPECT_FALSE(s.empty());
+    s.clear();
+    EXPECT_TRUE(s.empty());
+    EXPECT_EQ(s.size(), 0);
+}
+
+// ---------- shrink_to_fit ----------
+TEST(MethodTest, ShrinkToFit) {
+    String s("abc");
+    size_t oldCap = s.capacity();
+    s.shrink_to_fit();
+    EXPECT_EQ(s.capacity(), s.size() + 1);
+    EXPECT_LE(s.capacity(), oldCap);
+}
+
+// ---------- data ----------
+TEST(MethodTest, DataCheck) {
+    String s("hello");
+    EXPECT_STREQ(s.data(), "hello");
+    EXPECT_EQ(strcmp(s.data(), static_cast<const char*>(s)), 0);
+}
+
+// ---------- Ввод/вывод ----------
+TEST(IOTest, Output) {
+    String s("world");
+    std::ostringstream oss;
+    oss << s;
+    EXPECT_EQ(oss.str(), "world");
+}
+
+TEST(IOTest, Input) {
+    std::istringstream iss("hello world");
+    String s;
+    iss >> s;
+    EXPECT_EQ(s, "hello"_str);
+}
+
+// ---------- Литералы ----------
+TEST(LiteralTest, UserDefinedLiteral) {
+    String s = "test"_str;
+    EXPECT_EQ(s, String("test"));
+}
+
+//---------- Граничные случаи ----------
+TEST(BoundaryTest, LargeString) {
+    const size_t N = 10000;
+    String s;
+    for (size_t i = 0; i < N; i++) s.push_back('x');
+    EXPECT_EQ(s.size(), N);
+    for (size_t i = 0; i < 100; i++) s.pop_back();
+    EXPECT_EQ(s.size(), N - 100);
+}
+
+TEST(BoundaryTest, SingleChar) {
+    String s("a");
+    EXPECT_EQ(s.size(), 1);
+    EXPECT_EQ(s.front(), 'a');
+    EXPECT_EQ(s.back(), 'a');
+}
+
+TEST(BoundaryTest, OnlyNullChar) {
+    String s("\0");
+    EXPECT_EQ(s.size(), 0); // strlen("\0") == 0
+    EXPECT_TRUE(s.empty());
+}
